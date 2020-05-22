052d4643115fd3aa6611aad227435aeb40ce66e7
Steven G. Johnson <stevenj@alum.mit.edu>
2020-05-22 14:41:27 -0400

    refactoring

--- src/conversions.jl
+++ src/conversions.jl
@@ -1,7 +1,7 @@
 # conversion of Octave objects to/from Julia copies
 
 # alias for Cxx class T
-const OctType{T} = Cxx.CxxCore.CppPtr{<:Cxx.CxxCore.CppValue{<:Cxx.CxxCore.CxxQualType{Cxx.CxxCore.CppBaseType{T}}}}
+const OctType{T} = Cxx.CxxCore.CppPtr{<:Cxx.CxxCore.CppValue{<:Cxx.CxxCore.CxxQualType{<:Cxx.CxxCore.CppBaseType{T}}}}
 
 # aliases for std::complex<double> and std::complex<float>:
 const cxxComplexF64 = Cxx.CxxCore.CppValue{Cxx.CxxCore.CxxQualType{Cxx.CxxCore.CppTemplate{Cxx.CxxCore.CppBaseType{Symbol("std::__1::complex")},Tuple{Float64}},(false, false, false)},16}
@@ -17,6 +17,34 @@ jl2oct(x::Complex{Float64}) = icxx"std::complex<double>($(real(x)),$(imag(x)));"
 jl2oct(x::Complex{Float32}) = icxx"std::complex<float>($(real(x)),$(imag(x)));"
 oct2jl(o::Union{cxxComplexF64,cxxComplexF32}) = Complex(@cxx(o -> real()), @cxx(o -> imag()))
 
+# copy data between Julia and Octave arrays, with no checking
+function _unsafe_copy!(o::Cxx.CxxCore.CppPtr, x::AbstractArray{T}) where {T}
+    po = @cxx o -> fortran_vec()
+    # the Julia types above have the same binary
+    # layout as the corresponding C++ types, so
+    # converting to an explicit Julia pointer is
+    # safe and simplifies conversions
+    p = Ptr{T}(convert(Ptr{Cvoid}, po))
+    GC.@preserve o for i = 1:length(x)
+        unsafe_store!(p, x[i], i)
+    end
+    return o
+end
+function _unsafe_copy!(x::AbstractArray{T}, o::Cxx.CxxCore.CppPtr) where {T}
+    po = @cxx o -> fortran_vec()
+    p = Ptr{T}(convert(Ptr{Cvoid}, po))
+    GC.@preserve o for i = 1:length(x)
+        x[i] = unsafe_load(p, i)
+    end
+    return x
+end
+
+# analogue to size() for an octave array
+function octsize(o)
+    d = @cxx o -> dims() # dim_vector
+    GC.@preserve d ntuple(i -> unsafe_load(@cxx d -> elem(i-1)), @cxx d -> ndims())
+end
+
 for (J,O) in
     ((Float64,:Matrix),
      (Float32,:FloatMatrix),
@@ -24,32 +52,10 @@ for (J,O) in
      (ComplexF32,:FloatComplexMatrix),
      (Bool,:boolMatrix))
      @eval begin
-         function jl2oct(x::AbstractMatrix{$J})
-             o = @cxxnew $O(size(x,1), size(x,2))
-             po = @cxx o -> fortran_vec()
-             # the Julia types above have the same binary
-             # layout as the corresponding C++ types, so
-             # converting to an explicit Julia pointer is
-             # safe and simplifies conversions
-             p = Ptr{$J}(convert(Ptr{Cvoid}, po))
-             GC.@preserve o for i = 1:length(x)
-                 unsafe_store!(p, x[i], i)
-             end
-             return o
-         end
+         jl2oct(x::AbstractMatrix{$J}) =
+            _unsafe_copy!(@cxxnew($O(size(x,1), size(x,2))), x)
 
-         function oct2jl(o::OctType{$(QuoteNode(O))})
-             d = @cxx o -> dims() # dim_vector
-             GC.@preserve d begin
-                 m, n = unsafe_load(@cxx d -> elem(0)), unsafe_load(@cxx d -> elem(1))
-             end
-             x = Matrix{$J}(undef, m, n)
-             po = @cxx o -> fortran_vec()
-             p = Ptr{$J}(convert(Ptr{Cvoid}, po))
-             GC.@preserve o for i = 1:length(x)
-                 x[i] = unsafe_load(p, i)
-             end
-             return x
-         end
+         oct2jl(o::OctType{$(QuoteNode(O))}) =
+             return _unsafe_copy!(Matrix{$J}(undef, octsize(o)...), o)
      end
  end
